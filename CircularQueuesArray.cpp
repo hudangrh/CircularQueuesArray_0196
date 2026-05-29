54df28b33bb1bd1953601174cbbb20f7eb2e30a2
hudangrh <hudangrhkuliah@gmail.com>
2026-05-29 19:17:15 +0700

    CEK APAKAH ANTRIAN KOSONG

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -80,5 +80,12 @@ void display()
 {
     int FRONT_Position = FRONT;
     int REAR_Position = REAR;
-    
+
+
+     // cek apakah antrian kosong
+    if (FRONT_Position == -1)
+    {
+        cout << "Queue is empty\n";
+        return;
+    }
 }
