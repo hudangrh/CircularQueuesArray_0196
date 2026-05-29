20a689e1b1428b2ab72264a8fca0d6164f1a2cd4
hudangrh <hudangrhkuliah@gmail.com>
2026-05-29 19:11:27 +0700

    VOID REMOVE END

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -55,13 +55,23 @@ void remove()
        return;
     }
 
-     cout << "\nThe element deleted from the queue is: "
-             << queue_ARRAY[FRONT] << "\n";
+    cout << "\nThe element deleted from the queue is: "
+         << queue_ARRAY[FRONT] << "\n";
 
         // cek jika antrian hanya memiliki satu elemen
-        if (FRONT == REAR)
-        {
-            FRONT = -1;
-            REAR = -1;
-        }
+    if (FRONT == REAR)
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    else
+    {
+        // jika elemen yang dihapus berada di posisi terakhir array,
+            // kembali ke awal array
+        if (FRONT == max - 1)
+                FRONT = 0;
+        else
+                FRONT = FRONT + 1;
+    }
 }
